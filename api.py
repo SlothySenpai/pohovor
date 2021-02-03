6a7dc6f8a85f316a1a7fd6705c1aaca9ea6e4087
SlothySenpai <63344159+SlothySenpai@users.noreply.github.com>
2021-02-03 12:57:04 +0100

    Update api.py

--- api.py
+++ api.py
@@ -2,6 +2,6 @@ from discord.ext import commands
 
 
 client = commands.Bot(command_prefix='.')
-DISCORD_TOKEN = 'ODAxMTk4MzcyNzAxOTk1MDU4.YAdMIQ.hHwxmTuIc6efiwRL5i3FhiPuE6k'
-RIOT_API_TOKEN = 'RGAPI-b5b54495-43c1-4056-becf-462ce9921879'
+DISCORD_TOKEN = 'zde_discord_token'
+RIOT_API_TOKEN = 'zde_riot_api_token'
 EUN1_HTTP_LINK = 'https://eun1.api.riotgames.com/lol'
